0354995180a1f87528c6284e84eb1fd9a13fa519
psteens <patrick.steens@dxc.com>
2023-01-13 13:06:05 +0100

    Dockerfile + starting the AuditService

--- src/main/java/be/positive/calculator/positivesplitcalculator/PositiveSplitCalculatorApplication.java
+++ src/main/java/be/positive/calculator/positivesplitcalculator/PositiveSplitCalculatorApplication.java
@@ -1,8 +1,11 @@
 package be.positive.calculator.positivesplitcalculator;
 
+import brave.Tracer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 @Configuration
@@ -10,6 +13,16 @@ public class PositiveSplitCalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PositiveSplitCalculatorApplication.class, args);
+	}
+
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+		CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+		loggingFilter.setIncludeClientInfo(true);
+		loggingFilter.setIncludeQueryString(true);
+		loggingFilter.setIncludePayload(true);
+		loggingFilter.setMaxPayloadLength(64000);
+		return loggingFilter;
 	}
 
 }
